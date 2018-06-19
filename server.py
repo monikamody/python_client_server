cfda1ebfe6b1f97ac4925912d1c4fbe5cd326828
monikamody <38759347+monikamody@users.noreply.github.com>
2018-06-19 14:08:34 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python2
+
+#send Socket Creation
+
+import socket,sys
+
+
+s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+
+ip="192.168.10.158"
+port= 8888
+TIMEOUT=10
+
+
+s.bind((ip,port))
+s.settimeout(TIMEOUT)
+
+#while(4>2):
+#	t= s.recvfrom(40)
+#	print "rec from client" +t[0]
+#	s.sendto("hello",t[1])
+
+try :
+	while True :
+		r = s.recvfrom(1000)
+		print "receive from client : " + r[0]
+		reply = raw_input('server : ')
+		client_address = r[1]
+		s.sendto(reply, client_address)
+except:
+	print "timeout or no server running"
+	
